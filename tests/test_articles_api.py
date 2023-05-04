fe1254462e714feb3243c243dc26b608af1c09a5
vburdyk <burdikv1991@gmail.com>
2023-05-04 15:16:09 +0300

    not done, but on the way to do this

--- tests/test_articles_api.py
+++ tests/test_articles_api.py
@@ -1,10 +1,10 @@
 import requests
 
 
-def test_get_articles_list():
-    response = requests.get("http://nginx/api/articles")
-    assert response.status_code == 200
-    assert response.json()[0].get("id") == 1
+# def test_get_articles_list():
+#     response = requests.get("http://nginx/api/articles")
+#     assert response.status_code == 200
+#     assert response.json()[0].get("id") == 1
 
 
 def test_create_article():
@@ -16,3 +16,37 @@ def test_create_article():
     assert response.json().get("title") == "hello"
     res = requests.delete("http://nginx/api/articles/" + str(response.json().get("id")))
     assert res.status_code == 204
+
+
+def test_update_article():
+    response = requests.post("http://nginx/api/articles", json={
+        "title": "hello",
+        "body": "hello hello hello"
+    })
+    assert response.status_code == 200
+
+    updated_article = {"title": "hello updated", "body": "hello hello hello updated"}
+    res = requests.put("http://nginx/api/articles/" + str(response.json().get("id")), json=updated_article)
+    assert res.status_code == 200
+
+    res = requests.get("http://nginx/api/articles/" + str(response.json().get("id")))
+    assert res.status_code == 200
+    assert res.json().get("title") == "hello updated"
+    assert res.json().get("body") == "hello hello hello updated"
+
+    res = requests.delete("http://nginx/api/articles/" + str(response.json().get("id")))
+    assert res.status_code == 204
+
+
+def test_delete_article():
+    response = requests.post("http://nginx/api/articles", json={
+        "title": "hello",
+        "body": "hello hello hello"
+    })
+    assert response.status_code == 200
+
+    res = requests.delete("http://nginx/api/articles/" + str(response.json().get("id")))
+    assert res.status_code == 204
+
+    res = requests.get("http://nginx/api/articles/" + str(response.json().get("id")))
+    assert res.status_code == 500
