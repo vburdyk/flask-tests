12e8a549e7ded44452f243baa0e7749db0d8d21e
vburdyk <burdikv1991@gmail.com>
2023-05-05 13:53:57 +0300

    done

--- tests/test_articles_api.py
+++ tests/test_articles_api.py
@@ -1,10 +1,10 @@
 import requests
 
 
-# def test_get_articles_list():
-#     response = requests.get("http://nginx/api/articles")
-#     assert response.status_code == 200
-#     assert response.json()[0].get("id") == 1
+def test_get_articles_list():
+    response = requests.get("http://nginx/api/articles")
+    assert response.status_code == 200
+    assert response.json()[0].get("id") == 4
 
 
 def test_create_article():
@@ -50,3 +50,27 @@ def test_delete_article():
 
     res = requests.get("http://nginx/api/articles/" + str(response.json().get("id")))
     assert res.status_code == 500
+
+
+def test_get_category_list():
+    response = requests.get("http://nginx/api/categories")
+    assert response.status_code == 200
+    assert response.json()[0].get("id") == 1
+
+
+def test_get_menuitem():
+    response = requests.get("http://nginx/api/menu-items")
+    assert response.status_code == 200
+    assert response.json()[0].get("id") == 1
+
+
+def test_create_menuitem():
+    response = requests.post("http://nginx/api/menu-items", json={
+        "name": "hello",
+        "link": "helloo"
+    })
+    assert response.status_code == 200
+    assert response.json().get("name") == "hello"
+
+    res = requests.delete("http://nginx/api/menu-items/" + str(response.json()["id"]))
+    assert res.status_code == 404
